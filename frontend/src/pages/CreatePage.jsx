11115c3109c2f17f2a07086914e59a2369d8b725
Mireiden <miroslavucetic@gmail.com>
2025-04-28 23:35:26 +0200

    Created 'create product' form

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,7 +1,93 @@
-const CreatePage = () =>{
-    return{
+import {
+  Container,
+  VStack,
+  Box,
+  Heading,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+import { Toaster, toaster } from "@/components/ui/toaster";
+import { useColorModeValue } from "@/components/ui/color-mode";
+import React, { useState } from "react";
+import { useProductStore } from "@/store/product";
 
+const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+
+  const { createProduct } = useProductStore();
+
+  const handleAddProduct = async () => {
+    const { success, message } = await createProduct(newProduct);
+    if (!success) {
+      toaster.create({
+        title: "Error",
+        description: message,
+        status: "error",
+        isClosable: true,
+      });
+    } else {
+      toaster.create({
+        title: "Success",
+        description: message,
+        status: "success",
+        closable: true,
+      });
     }
-}
+  };
+
+  return (
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create New Product
+        </Heading>
+
+        <Box
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack spacing={4}>
+            <Input
+              placeholder="Product name"
+              name="name"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Price"
+              name="price"
+              type="number"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Image URL"
+              name="image"
+              value={newProduct.image}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            />
+            <Button colorPalette="blue" onClick={handleAddProduct} w="full">
+              Add Product
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+      <Toaster />
+    </Container>
+  );
+};
 
 export default CreatePage;
